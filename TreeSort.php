76c06fb2c399365c3092df331e13bbd2c78451f9
Albert van der Meer <avandermeer@avdm.nl>
2018-03-02 15:38:57 +0100

    sort tree tools added

---
+++ TreeSort.php
@@ -0,0 +1,62 @@
+ class TreeSort{
+
+
+ public static function treeBuilder($currentId, $parentId, $root=null)
+    {
+
+        if($root==null){
+            $root = [];
+        }
+
+        //check if $parentId exists in tree:
+        $routeToParent = static::recursiveKeyFinder($parentId, [],  $root);
+
+
+        //else, or if $parentId is null
+        if(!$routeToParent){
+
+            //add to root
+            $root[$parentId] = [
+                $currentId => []
+            ];
+
+        }
+        else{
+
+        }
+
+        return $root;
+
+
+    }
+
+    /**
+     * @param $searchKey
+     * @param $route
+     * @param $array
+     * @return bool|mixed
+     */
+    public static function recursiveKeyFinder($searchKey, $route, $array){
+        foreach($array as $key => $child){
+            if($key == $searchKey){
+                return $route;
+            }
+            else{
+                if(!empty($child)){
+                    array_push($route, $key);
+                    $foundRoute = static::recursiveKeyFinder($searchKey, $route, $child);
+                    if($foundRoute){
+                        return $foundRoute;
+                    }
+                    else{
+                       array_pop($route);
+                    }
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+}
